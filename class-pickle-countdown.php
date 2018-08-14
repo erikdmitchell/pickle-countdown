b87da661af0f3fc05495ef2c1d725456098bb660
Erik <erikdmitchell@gmail.com>
2018-08-14 14:55:21 -0400

    cleaned up code

--- class-pickle-countdown.php
+++ class-pickle-countdown.php
@@ -12,44 +12,98 @@
  * @final
  */
 final class Pickle_Countdown {
-	
-	public $version = '1.0.0';
-	
-	public function __construct() {
-		$this->define_constants();
-		$this->includes();
-		$this->init_hooks();		
-	}
 
-	private function define_constants() {
-		define('PICKLE_COUNTDOWN_PATH', plugin_dir_path(__FILE__));
-		define('PICKLE_COUNTDOWN_URL', plugin_dir_url(__FILE__));
-	}
+    /**
+     * Version
+     *
+     * (default value: '1.0.0')
+     *
+     * @var string
+     * @access public
+     */
+    public $version = '1.0.0';
 
-	public function includes() {
+    /**
+     * __construct function.
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct() {
+        $this->define_constants();
+        $this->includes();
+        $this->init_hooks();
+    }
 
-	}
+    /**
+     * Define constants.
+     *
+     * @access private
+     * @return void
+     */
+    private function define_constants() {
+        define( 'PICKLE_COUNTDOWN_PATH', plugin_dir_path( __FILE__ ) );
+        define( 'PICKLE_COUNTDOWN_URL', plugin_dir_url( __FILE__ ) );
+    }
 
-	private function init_hooks() {
-		add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts_styles'));
-		add_shortcode('pickle_countdown', array($this, 'shortcode'));
-	}
+    /**
+     * Includes.
+     *
+     * @access public
+     * @return void
+     */
+    public function includes() {
 
-	private function define( $name, $value ) {
-		if ( ! defined( $name ) ) {
-			define( $name, $value );
-		}
-	}
+    }
 
-	public function enqueue_scripts_styles() {
-		wp_register_script('jquery-countdown-script', PICKLE_COUNTDOWN_URL.'js/jquery.countdown.min.js', array('jquery'), '2.2.0', true);
-		wp_register_script('pickle-countdown-timer-settings-script', PICKLE_COUNTDOWN_URL.'js/timerSettings.js', array('jquery-countdown-script'), $this->version, true);	
-	}
-	
-	public function shortcode($atts) {		
-		extract(shortcode_atts(array(
-			'date' => '2020/01/01',
-			'format' => '<div class="boomi-countdown">
+    /**
+     * Init hooks.
+     *
+     * @access private
+     * @return void
+     */
+    private function init_hooks() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
+        add_shortcode( 'pickle_countdown', array( $this, 'shortcode' ) );
+    }
+
+    /**
+     * Define function.
+     *
+     * @access private
+     * @param mixed $name string.
+     * @param mixed $value string.
+     * @return void
+     */
+    private function define( $name, $value ) {
+        if ( ! defined( $name ) ) {
+            define( $name, $value );
+        }
+    }
+
+    /**
+     * Enqueue frontend scripts and styles.
+     *
+     * @access public
+     * @return void
+     */
+    public function enqueue_scripts_styles() {
+        wp_register_script( 'jquery-countdown-script', PICKLE_COUNTDOWN_URL . 'js/jquery.countdown.min.js', array( 'jquery' ), '2.2.0', true );
+        wp_register_script( 'pickle-countdown-timer-settings-script', PICKLE_COUNTDOWN_URL . 'js/timerSettings.js', array( 'jquery-countdown-script' ), $this->version, true );
+    }
+
+    /**
+     * Shortcode output.
+     *
+     * @access public
+     * @param mixed $atts (array).
+     * @return html
+     */
+    public function shortcode( $atts ) {
+        shortcode_atts(
+            array(
+                'date' => '2020/01/01',
+                'format' => '<div class="boomi-countdown">
 			<div>
 				<div class="count-number">%D</div>
 				<div class="count-text">DAYS</div>
@@ -70,26 +124,29 @@ final class Pickle_Countdown {
 				<div class="count-text">SECONDS</div>
 			</div>
 		</div>',
-		), $atts, 'pickle-countdown'));
-		
-		wp_localize_script('pickle-countdown-timer-settings-script', 'pcTimerOptions', array(
-			'date' => $date,
-			'format' => $format,
-		));
-		
-		$html='';
-		
-		$html.='<div id="pickle-countdown">';
-			$html.='<div class="timer"></div>';
-			$html.='<div class="text">'.$text.'</div>';
-		$html.='</div>';
-		
-		wp_enqueue_script('jquery-countdown-script');
-		wp_enqueue_script('pickle-countdown-timer-settings-script');
-				
-		return $html;
-	}
-		
+            ), $atts, 'pickle-countdown'
+        );
+
+        wp_localize_script(
+            'pickle-countdown-timer-settings-script', 'pcTimerOptions', array(
+                'date' => $atts['date'],
+                'format' => $atts['format'],
+            )
+        );
+
+        $html = '';
+
+        $html .= '<div id="pickle-countdown">';
+            $html .= '<div class="timer"></div>';
+            $html .= '<div class="text">' . $text . '</div>';
+        $html .= '</div>';
+
+        wp_enqueue_script( 'jquery-countdown-script' );
+        wp_enqueue_script( 'pickle-countdown-timer-settings-script' );
+
+        return $html;
+    }
+
 }
 
 /**
@@ -99,7 +156,7 @@ final class Pickle_Countdown {
  * @return class
  */
 function pickle_countdown() {
-    return new Pickle_Countdown();();
+    return new Pickle_Countdown();
 }
 
 // Global for backwards compatibility.
--- js/timerSettings.js
+++ js/timerSettings.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	
+
 	$("#pickle-countdown .timer").countdown(pcTimerOptions.date, function(event) {
 		$(this).html(event.strftime(pcTimerOptions.format));
 	});	
