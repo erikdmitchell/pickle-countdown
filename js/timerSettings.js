93813d88ddb353d5e46230d453c9ee003a84dbb8
Erik <erikdmitchell@gmail.com>
2019-07-08 16:47:10 -0400

    lintjs

--- js/timerSettings.js
+++ js/timerSettings.js
@@ -1,6 +1,8 @@
+/* global pcTimerOptions */
+
 jQuery(document).ready(function($) {
 
-	$("#pickle-countdown .timer").countdown(pcTimerOptions.date, function(event) {
+	$('#pickle-countdown .timer').countdown(pcTimerOptions.date, function(event) {
 		$(this).html(event.strftime(pcTimerOptions.format));
 	});	
 	
