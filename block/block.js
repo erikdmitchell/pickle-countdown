59bcb5ee58b708f84f833c8c03c832b5c4a4fc3b
Erik <erikdmitchell@gmail.com>
2021-05-11 11:05:09 -0400

    added countdown time

--- block/block.js
+++ block/block.js
@@ -12,9 +12,6 @@
     	Panel,
     	PanelBody
     } = components;
-    
-    var today = new Date();
-console.log(today);
  
     blocks.registerBlockType( 'pickle-countdown/block', {
         title: 'Pickle Countdown',
@@ -83,3 +80,29 @@ console.log(today);
     window.wp.i18n,
     window.wp.element
 ) );
+
+function countdownTimer() {
+    const difference = +new Date("2020-01-01") - +new Date();
+    let remaining = "Time's up!";
+    
+    if (difference > 0) {
+      const parts = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    
+      remaining = Object.keys(parts)
+        .map(part => {
+          if (!parts[part]) return;
+          return `${parts[part]} ${part}`;
+        })
+        .join(" ");
+    }
+    
+    document.getElementById("countdown").innerHTML = remaining;
+}
+
+countdownTimer();
+setInterval(countdownTimer, 1000);
